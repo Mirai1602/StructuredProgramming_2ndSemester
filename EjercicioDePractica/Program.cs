b6d7c5961c2b56d95be9fde0d6a3f2a0813bd049
Mirai <158864225+Mirai1602@users.noreply.github.com>
2025-08-31 11:48:47 -0600

    Add files via upload
    Ver si se abre

--- EjercicioDePractica/Program.cs
+++ EjercicioDePractica/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Programa que permite calcular la suma independiente de los numero pares e impares entre el 1 al 100");
+int i;
+int sumaPares, sumaImpares =0;
+for (i = 1; i <= 100; i = i + 2)
+{
+    sumaPares = sumaPares + i;
+    Console.WriteLine("La suma de los numeros pares es: " + sumaPares);
+}
+for (i = 1; i <= 100; i = i + 2)
+{
+    sumaImpares = sumaImpares + i;
+    Console.WriteLine("La suma de los numeros impares es: " + sumaImpares);
+}
+for (i = 1; i <= 100; i++)
+{
+    if (i % 2 == 0)
+    {
+        sumaPares += i;
+        Console.WriteLine("La suma de los numeros pares es: " + sumaPares);
+    }
+    else
+    {
+        sumaImpares += i;
+        Console.WriteLine("La suma de los numeros impares es: " + sumaImpares);
+    }
+}
