5815c35eaec009d253f4bce2d77cfeaa18df9da4
Mirai <158864225+Mirai1602@users.noreply.github.com>
2025-08-31 11:46:10 -0600

    Add files via upload
    Este doc en mi vs si se abre y esta completo

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Programa para pedir la nota de un estudiante y mostrar que aprobo (mas de 70), recuperacion (50-69) o reprueba (menos de 50)");
+int nota;
+Console.WriteLine("Ingrese la nota del estudiante");
+nota = Convert.ToInt32(Console.ReadLine());
+if (nota >= 70)
+{
+    Console.WriteLine("El estudiante está en aprobo");
+}
+else if (nota >= 50 && nota < 70)
+{
+    Console.WriteLine("EL estudiante debe ir a reparacion");
+}
+else
+{
+    Consoloe.WriteLine("El estudiante reprueba");
+}
+
