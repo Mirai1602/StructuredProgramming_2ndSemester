01e9048ce1d746a6198034e95f822a5658aaced6
Mirai <158864225+Mirai1602@users.noreply.github.com>
2025-08-31 11:48:17 -0600

    Add files via upload
    Vean si se abre

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Este programa nos permite determinar si un año es bisiesto o no");
+int year;
+Console.WriteLine("Por favor ingresa un año:");
+year = Convert.ToInt32(Console.ReadLine());
+
+if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+{
+    Console.WriteLine("El año " + year + " es bisiesto.");
+}
+else
+{
+    Console.WriteLine("El año " + year + " no es bisiesto.");
+}
