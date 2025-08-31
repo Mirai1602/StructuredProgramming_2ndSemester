3b4cd923a4afd16b7543510fd50d9fd432f81b7c
Mirai <158864225+Mirai1602@users.noreply.github.com>
2025-08-31 11:47:27 -0600

    Add files via upload
    Se supone que este tambien deberia de abrir

--- Ejercicio2.1/Program.cs
+++ Ejercicio2.1/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Programa para pedir la nota de un estudiante y mostrar que aprobo (mas de 70), recuperacion (50-69) o reprueba (menos de 50) con switch case");
+int nota;
+Console.WriteLine("Ingrese la nota del estudiante");
+nota = Convert.ToInt32(Console.ReadLine());
+switch (nota) //Obtiene como parametro el valor de nota
+{
+    case int n when (n >= 70):
+        Console.WriteLine("El estudiante aprobo");
+        break;
+    case int n when (n >= 50 && n < 70):
+        Console.WriteLine("El estudiante debe ir a reparacion");
+        break;
+    default:
+        Console.WriteLine("El estudiante reprueba");
+        break;
+}
